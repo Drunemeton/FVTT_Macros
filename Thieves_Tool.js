b2c6d8615fc3f58a7bf1d08292adec60e67bcc56
Drunemeton <63975886+Drunemeton@users.noreply.github.com>
2020-08-29 16:33:43 -0700

    Delete Thieves_Tool.js

--- Thieves_Tool.js
+++
@@ -1,8 +0,0 @@
-let flavor = "<h3>Is attempting to use his Thieves’ Tools…</h3>";
-let formula = "1d20 + @abilities.dex.mod + @prof";
-new Roll(formula, token.actor.getRollData()).roll().toMessage({
-  speaker: ChatMessage.getSpeaker({
-    actor: this.actor,
-  }),
-  flavor: flavor,
-});
